877a64ed967c2809b6da51413e43b3769a81902b
Konstantin Gonikman <info@alstertouch.com>
2024-02-26 21:43:07 +0100

    Initial Commit

---
+++ bonprix/bonprixApp.swift
@@ -0,0 +1,17 @@
+//
+//  bonprixApp.swift
+//  bonprix
+//
+//  Created by Konstantin Gonikman on 26.02.24.
+//
+
+import SwiftUI
+
+@main
+struct bonprixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
